45f0e59efffdca006fbf5ee75e072b28b6ff2beb
NhuNhui <giaqui1403@gmail.com>
2024-01-11 16:35:09 +0700

    add gameover and play again

--- beat_mouse_game.py
+++ beat_mouse_game.py
@@ -70,8 +70,11 @@ score_font = pygame.font.Font(None,72)
 def display_score():
     text_score = score_font.render(str(score),True,(255,255,255))
     screen.blit(text_score,(800,10))
-
-
+    
+################# set game over ###################################
+gameOver = pygame.image.load(r'Image\game_over.png')
+gameOver = pygame.transform.scale(gameOver,(500,400))
+gameOver_rect = gameOver.get_rect(center=(x_screen/2,y_screen/2))
 #set timer
 timer_interrup = pygame.USEREVENT
 pygame.time.set_timer(timer_interrup,100)
@@ -79,6 +82,14 @@ pygame.time.set_timer(timer_interrup,100)
 flag_appear = 0
 flag_disappear = 0
 
+
+timer_countdown = 60
+timer_countdown_font = pygame.font.Font(None,72)
+def display_timer_countdown():
+    text_timer_countdown = timer_countdown_font.render(str(timer_countdown),True,(0,0,0))
+    screen.blit(text_timer_countdown,(500,10))
+    
+game_play = True
 
 ######################### game loop ###########################
 counter_list = []
@@ -88,10 +99,11 @@ while run:
         if(event.type == pygame.QUIT):
             run = False
             
-        if(event.type == timer_interrup):    
+        if(event.type == timer_interrup and game_play):    
             flag_appear += 1
             flag_disappear += 1
-            if(flag_appear >= 10): #mỗi 700s sẽ xuất hiện con chuột
+            if(flag_appear >= 10): #mỗi 1s sẽ xuất hiện con chuột
+                timer_countdown -= 1
                 flag_appear = 0
                 #todo
                 counter = random.randint(0,12)
@@ -100,7 +112,7 @@ while run:
                 list_mouse_appear.append(hole_mouse[counter])
                 hole_have_mouse.append(hole_list[counter])
             
-            if(flag_disappear >= 20): #con chuột xuất hiện mỗi 1000s r biến mất  
+            if(flag_disappear >= 20): #con chuột xuất hiện mỗi 2s r biến mất  
                 flag_disappear = 0
                 if(len(list_mouse_appear) > 0):
                     list_mouse_appear.pop(0)
@@ -110,32 +122,52 @@ while run:
             
         if event.type == pygame.MOUSEBUTTONDOWN:
             # Xử lý khi có click chuột
-            click_sound.play()
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            for hole in hole_have_mouse:
-                #xử lí khi đập trúng chuột
-                if (mouse_x >= hole[2] and mouse_x <= hole[3]) and (mouse_y >= hole[0] and mouse_y <= hole[1]):
-                    score += 1
-                    print(f'Score: {score}')
-                    hit_sound.play()
-                    # xử lí sau khi đập
-                    index_delete = hole_list.index(hole)
-                    hole_have_mouse.remove(hole)
-                    list_mouse_appear.remove(hole_mouse[index_delete])
-                    break
+            
+            if(game_play):
+                click_sound.play()
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                # print(f'mouse: x {mouse_x}, y {mouse_y}')
+                for hole in hole_have_mouse:
+                    #xử lí khi đập trúng chuột
+                    if (mouse_x >= hole[2] and mouse_x <= hole[3]) and (mouse_y >= hole[0] and mouse_y <= hole[1]):
+                        score += 1
+                        print(f'Score: {score}')
+                        hit_sound.play()
+                        # xử lí sau khi đập
+                        index_delete = hole_list.index(hole)
+                        hole_have_mouse.remove(hole)
+                        list_mouse_appear.remove(hole_mouse[index_delete])
+                        break
+            else:
+                
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if(mouse_x > 317 and mouse_x < 400) and (mouse_y > 544 and mouse_y < 576):
+                    game_play = True
+                    score = 0
+                if((mouse_x > 524 and mouse_x < 588) and (mouse_y > 543 and mouse_y < 577)):
+                    run = False
+                # print(f'mouse: x {mouse_x}, y {mouse_y}')
             
             
     #draw background     
     screen.blit(bg,(0,0))
     screen.blit(bg2,(0,0))
+    if(game_play):
+        
+        #randon draw mouse
+        display_mouse()
+        
+        
+        #dwaw hammer
+        hammer_rect.center = pygame.mouse.get_pos()
+        screen.blit(hammer,hammer_rect)
+        display_score()
+        display_timer_countdown()
+        if(timer_countdown <= 0):
+            game_play = False
+            timer_countdown = 60
     
-    #randon draw mouse
-    display_mouse()
-    
-    
-    #dwaw hammer
-    hammer_rect.center = pygame.mouse.get_pos()
-    screen.blit(hammer,hammer_rect)
-    display_score()
+    else:
+        screen.blit(gameOver,gameOver_rect)
     
     pygame.display.update()
