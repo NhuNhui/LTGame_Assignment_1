d03ffd4b1746a121fe5242c6b63d75c74e37aeb2
Huỳnh Gia Qui <145247195+NhuNhui@users.noreply.github.com>
2024-01-29 01:35:39 +0700

    Update beat_mouse_game.py

--- beat_mouse_game.py
+++ beat_mouse_game.py
@@ -43,13 +43,15 @@ mouse = pygame.transform.scale(mouse,(110,110))
 holes = [[75,180],[470,180],[740,180],[250,240],[430,300],[670,270]
          ,[40,300],[210,420],[720,390],[90,540],[350,620],[690,570],[470,470]]
 for hole in holes:
-    hole[0] += 55
+    hole[0] += 55 #hole center
     hole[1] += 55
     
 hole_mouse = []
-for hole in holes:
+for hole in holes: # vi tri con chuot neu no dung o tam cai lo
     hole_mouse.append(mouse.get_rect(center = (hole[0],hole[1])))
-    
+
+ini_y = [mouse_rect.copy() for mouse_rect in hole_mouse]
+
 hole_list = []
 for hole in holes:
     top, down, left, right = hole[1] - 55, hole[1] + 55, hole[0] - 55, hole[0] + 55 
@@ -57,38 +59,22 @@ for hole in holes:
 
 list_mouse_appear = []
 hole_have_mouse = []
-# for hole in holes:
-#     # print(i)
-#     random_mouse = mouse.get_rect(center = (hole[0],hole[1]))
-#     list_mouse_appear.append(random_mouse)
-# print(hole_list)
 
-def display_mouse():
-    for mouse_appear in list_mouse_appear:
-        screen.blit(mouse,mouse_appear)
+
+        
 
 ################## end set mouse and hole ############################
 
 #Hammer
 hammer = pygame.image.load(r'Image\Hammer.png')
 hammer = pygame.transform.scale(hammer,(90,90))
-hammer_rect = hammer.get_rect(center=(x_screen/2,y_screen/2))
+hammer_rect = hammer.get_rect(center=(x_screen,y_screen))
 
 #sound Hammer
 click_sound = pygame.mixer.Sound(r'Sound\sfx_wing.wav') 
 hit_sound = pygame.mixer.Sound(r'Sound\sfx_hit.wav') 
 back_ground_sound = pygame.mixer.Sound(r'Sound\01-FAIRY-TAIL-Main-Theme-Takanashi-Yasuharu.mp3') 
 back_ground_sound.set_volume(0.01)
-#hammer animation
-original_hammer_frames  = [pygame.image.load(r'Animation\hammer_ani\0001.png'), pygame.image.load(r'Animation\hammer_ani\0002.png'), pygame.image.load(r'Animation\hammer_ani\0003.png'), pygame.image.load(r'Animation\hammer_ani\0004.png'),pygame.image.load(r'Animation\hammer_ani\0005.png')]
-hammer_frames = [pygame.transform.scale(frame, (90, 90)) for frame in original_hammer_frames]
-current_hammer_frame = 0
-frame_counter = 0
-frame_threshold = 10
-# Flag to control animation
-play_animation = False
-show_static_hammer = True
-pygame.mouse.set_visible(False)
 #Score
 score = 0
 score_font = pygame.font.Font(None,72)
@@ -126,45 +112,76 @@ game_play = True
 
 ######################### game loop ###########################
 counter_list = []
+list_go_down = []
 run = True
 while run:
+    
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             run = False
-            
-            
+
+        def display_mouse():
+                for mouse_appear in list_mouse_appear:
+                    screen.blit(mouse, mouse_appear)  
         if(event.type == timer_interrup and game_play and start_game_display == False):    
             flag_appear += 1
             flag_disappear += 1
+            
             if(flag_appear >= 10): #mỗi 1s sẽ xuất hiện con chuột
                 timer_countdown -= 1
-                flag_appear = 0
-                #todo
+                flag_appear = 0        
                 counter = random.randint(0,12)
                 counter_list.append(counter)
-                
+
                 list_mouse_appear.append(hole_mouse[counter])
                 hole_have_mouse.append(hole_list[counter])
+                print("xuat hien o so")
+                print(counter)
             
-            if(flag_disappear >= 20): #con chuột xuất hiện mỗi 2s r biến mất  
+            for my in counter_list:
+                if hole_mouse[my].y + hole_mouse[my].height > holes[my][1]+40:
+                    hole_mouse[my].y -=4
+            
+            #counter list laf ddi len    
+            if(flag_disappear >= 20) and len(counter_list) > 2: #con chuột xuất hiện mỗi 2s r biến mất  
                 flag_disappear = 0
-                if(len(list_mouse_appear) > 0):
-                    list_mouse_appear.pop(0)
+                #print("go down")
+                #print(counter_list[0])
+                list_go_down.append(counter_list[0])
+                counter_list.pop(0)
+                    
                 if(len(hole_have_mouse) > 0):
                     hole_have_mouse.pop(0)
-        
+
+            delHoleInd=[]
+            if len(list_go_down) > 0:
+                for my in list_go_down:
+                    if hole_mouse[my].y < ini_y[my].y :
+                        hole_mouse[my].y += 8
+                    else:
+                        delHoleInd.append(my)
+                        #print("them o xoa")
+                        #print (my)
+
+
+            # del mouses in o so my
+            if len(delHoleInd) > 0:
+                for ind in delHoleInd:
+                    if(len(list_mouse_appear) > 0):
+                        list_mouse_appear.remove(hole_mouse[ind])
+                        #print("xoa o so")
+                        #print(ind)
+
+                    list_go_down.remove(ind)
+
+            delHoleInd.clear()
             
         if event.type == pygame.MOUSEBUTTONDOWN:
             # Xử lý khi có click chuột
             if(start_game_display == False):
-                if game_play and not play_animation:
-                    play_animation = True
-                    show_static_hammer = False
+                if(game_play):
                     click_sound.play()
                     mouse_x, mouse_y = pygame.mouse.get_pos()
-                    frame_counter = 0
-                    current_hammer_frame = 1
-                    
                     # print(f'mouse: x {mouse_x}, y {mouse_y}')
                     for hole in hole_have_mouse:
                         #xử lí khi đập trúng chuột
@@ -174,11 +191,15 @@ while run:
                             hit_sound.play()
                             # xử lí sau khi đập
                             index_delete = hole_list.index(hole)
+                            #index_delete laf ten lo can remove
                             hole_have_mouse.remove(hole)
+
                             list_mouse_appear.remove(hole_mouse[index_delete])
+                            hole_mouse[index_delete].y=ini_y[index_delete].y
+                            counter_list.remove(index_delete)
+
                             break
                 else:
-                    
                     mouse_x, mouse_y = pygame.mouse.get_pos()
                     if(mouse_x > 317 and mouse_x < 400) and (mouse_y > 544 and mouse_y < 576):
                         game_play = True
@@ -188,19 +209,19 @@ while run:
                     # print(f'mouse: x {mouse_x}, y {mouse_y}')
             
             else:
-                
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 if (mouse_x >= 343 and mouse_x <= 557) and (mouse_y >= 555 and mouse_y <= 646):
                     start_game_display = False
                     game_play = True
                     score = 0
                     timer_countdown = 60
-                         
-
+                
+                
+       
     #343 -> 557; 555 # y: 555 -> 646
+                    
     #draw background
     if(start_game_display):
-        pygame.mouse.set_visible(True)
         back_ground_sound.play()   
         white = (255, 255, 255)
         screen.fill(white)
@@ -211,7 +232,6 @@ while run:
             screen.blit(start_game2,start_game2_rect)
         
     else:
-        pygame.mouse.set_visible(False)
         back_ground_sound.stop()
         screen.blit(bg,(0,0))
         screen.blit(bg2,(0,0))
@@ -227,34 +247,15 @@ while run:
             
             #dwaw hammer
             hammer_rect.center = pygame.mouse.get_pos()
-            if show_static_hammer:
-                screen.blit(hammer,hammer_rect)
-            if play_animation:
-                # Draw the hammer with animation
-                screen.blit(hammer_frames[current_hammer_frame], hammer_rect)
-
-                # Update hammer animation frames
-                frame_counter += 1
-                if frame_counter >= frame_threshold:
-                    current_hammer_frame = (current_hammer_frame + 1) % len(hammer_frames)
-                    frame_counter = 0
-
-                # Stop the animation after playing once
-                if current_hammer_frame == 0:
-                    play_animation = False
-                    show_static_hammer = True
-            
+            screen.blit(hammer,hammer_rect)
             
             if(timer_countdown <= 0):
                 game_play = False
                 
         
         else:
-            play_animation = False
-            pygame.mouse.set_visible(True)
             screen.blit(gameOver,gameOver_rect)
             hole_have_mouse.clear()
             list_mouse_appear.clear()
     
     pygame.display.update()
-pygame.mouse.set_visible(True)
